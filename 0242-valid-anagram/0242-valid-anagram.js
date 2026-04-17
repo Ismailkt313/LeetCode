d73a44fb0ca02b09dfbc4ab3aa9c1273fe3938f9
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-04-17 09:50:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -6,7 +6,6 @@
 var isAnagram = function(s, t) {
     let obj = {}
     let obj1 = {}
-    console.log(Object.entries(obj).length,Object.entries(obj1).length)
     for(let val of s) obj[val]=(obj[val]||0)+1
     for(let val of t) obj1[val]=(obj1[val]||0)+1
     if(Object.entries(obj).length != Object.entries(obj1).length){
