60d0157d3b685479721b0c725d98447aab12d533
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-04-20 10:20:20 +0530

    Time: 2 ms (78.79%), Space: 68.6 MB (9.09%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    let i = 0
+    let j = 0
+    let sum = 0
+    while(i < nums1.length){
+        if(nums1[i] <= nums2[j] ){
+            if((j-i) > sum){
+                sum = j-i
+            }
+            j++
+        }else{
+            i++
+            // j=i
+        }
+    }
+    return sum
+};
