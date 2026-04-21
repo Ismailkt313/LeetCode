43c4b63e8137dbf9d1b210a945e6c52b488a16c7
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-04-21 11:01:38 +0530

    Time: 6 ms (64.48%), Space: 69.6 MB (72.96%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let answer = new Array(nums.length).fill(1)
+    let prefix = 1
+    for(let i = 0 ; i < nums.length ; i++){
+        answer[i] *= prefix
+        prefix *= nums[i]
+    }
+    let sufix = 1
+    for(let i = nums.length-1 ; i >= 0 ; i--){
+        answer[i] *= sufix
+        sufix *= nums[i]
+    }
+    return (answer)
+};
