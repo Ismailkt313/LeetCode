ea4255fd7449894292a772016a68d9b86018447d
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-05-11 09:32:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -3,15 +3,5 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let arr = []
-    for(let i = 0 ; i < nums.length ; i++){
-        if(nums[i] > 9){
-            let val = nums[i].toString().split('')
-            let num = val.map(a=>Number(a))
-            arr = [...arr,...num]
-        }else{
-            arr.push(nums[i])
-        }
-    }
-    return arr
+    return nums.join('').split('').map(a=>Number(a))
 };
