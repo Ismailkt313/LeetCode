88b5ed93f88c30a4ea01750e5a50e259d28f7f6f
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-07-18 11:50:18 +0530

    Time: 0 ms (100%), Space: 54 MB (70.04%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+     let max = Math.max(...nums)
+     let min = Math.min(...nums)
+     if(max === min) return min
+     for(let i= min ; i >= 1 ; i-- ){
+        if(min % i === 0 && max % i === 0 ){
+           return i
+        }
+     }
+};
