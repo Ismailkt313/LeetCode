7c0ae9c51256fca1b87105acd5aece372df68f7f
muhammed ismail kt <muhammedismailkt@gmail.com>
2026-08-03 09:40:58 +0530

    Time: 758 ms (48.44%), Space: 191.7 MB (45.66%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    let adjList = {}
+    if(!edges.length){return true}
+    let seen = new Set()
+    for(let val of edges){
+        if(!adjList[val[0]]){
+            adjList[val[0]] = new Set()
+        }
+        if(!adjList[val[1]]){
+            adjList[val[1]] = new Set()
+        }
+        adjList[val[0]].add(val[1])
+        adjList[val[1]].add(val[0])
+    }
+    let reached = false
+     function dfs(start,graph,seen){
+        if(start == destination){
+            console.log("reached herer")
+            reached =  true
+        }
+        for(let val of graph[start]){
+            if(!seen.has(val)){
+                seen.add(val)
+                dfs(val,graph,seen)
+            }
+        }
+        return reached
+    }
+    let ans = dfs(source,adjList,seen)
+    return ans
+};
